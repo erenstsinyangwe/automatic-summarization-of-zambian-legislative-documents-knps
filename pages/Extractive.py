12774c257a1fd7a22150e17c4b321786deb5ba8e
ernestsinyangwe <107695432+erenstsinyangwe@users.noreply.github.com>
2023-10-30 21:21:00 +0200

    Update Extractive.py

--- pages/Extractive.py
+++ pages/Extractive.py
@@ -1,3 +1,5 @@
+
+python
 import streamlit as st
 import requests
 from io import BytesIO
@@ -6,79 +8,83 @@ import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 import nltk
 import string
-from heapq import nlargest
 
 # Download NLTK data for tokenization
 nltk.download('punkt')
 
-# Declare a global variable to store the extracted text
-file_content = None
-
-# Define the Streamlit app title
-st.title("Zambian Automatic Legislative Document Summarizer")
-
-# Create a text input field for the PDF URL
-pdf_url = st.text_input("Enter PDF URL:")
-
-# Create radio buttons for summarization percentage
-summarization_percentage = st.radio("Select Summarization Percentage", [10, 20, 25, 30, 50])
-
 # Define a function to extract text from a PDF URL
 def extract_text_from_url(pdf_url):
-    try:
-        # Fetch the PDF content from the provided URL
-        response = requests.get(pdf_url)
-        response.raise_for_status()
+    response = requests.get(pdf_url)
+    response.raise_for_status()
 
-        # Convert the PDF content into a readable text format
-        pdf_stream = BytesIO(response.content)
-        pdf_text = extract_text(pdf_stream)
+    pdf_stream = BytesIO(response.content)
+    pdf_text = extract_text(pdf_stream)
 
-        return pdf_text
-    except Exception as e:
-        return None
+    return pdf_text
 
-# Check if the "Summarize" button is clicked
-if st.button("Summarize"):
-    pdf_text = extract_text_from_url(pdf_url)
-    file_content = pdf_text
+# Define a function to calculate word frequencies
+def calculate_word_frequencies(doc):
+    stopwords = list(STOP_WORDS)
+    word_frequencies = {}
+    for word in doc:
+        if word.text.lower() not in stopwords and word.text.lower() not in string.punctuation:
+            if word.text not in word_frequencies:
+                word_frequencies[word.text] = 1
+            else:
+                word_frequencies[word.text] += 1
+    return word_frequencies
 
-    if file_content:
-        st.text("Summarized Text:")
+# Define a function to calculate sentence scores for summarization
+def calculate_sentence_scores(sentence_tokens, word_frequencies):
+    sentence_scores = {}
+    for sent in sentence_tokens:
+        score = sum(word_frequencies.get(word.text.lower(), 0) for word in sent)
+        sentence_scores[sent] = score
 
-        # Prepare NLP processing
-        stopwords = list(STOP_WORDS)
-        nlp = spacy.load('en_core_web_sm')
-        doc = nlp(file_content)
+    return sentence_scores
 
-        # Calculate word frequencies
-        word_frequencies = {}
-        for word in doc:
-            if word.text.lower() not in stopwords and word.text.lower() not in string.punctuation:
-                if word.text not in word_frequencies.keys():
-                    word_frequencies[word.text] = 1
-                else:
-                    word_frequencies[word.text] += 1
+# Define a function to generate a summary
+def generate_summary(sentence_tokens, sentence_scores, select_length):
+    sorted_sentences = sorted(sentence_tokens, key=lambda x: sentence_scores[x], reverse=True)
+    summary = sorted_sentences[:select_length]
 
-        # Find the maximum word frequency
-        max_frequency = max(word_frequencies.values())
+    return summary
 
-        # Normalize word frequencies
-        for word in word_frequencies.keys():
-            word_frequencies[word] = word_frequencies[word] / max_frequency
+# Define the Streamlit app
+if __name__ == '__main__':
+    # Set the page configuration
+    st.set_page_config(
+        page_title="Zambian Automatic Legislative Document Summarizer",
+        page_icon="📜"
+    )
 
-        # Tokenize text into sentences
-        sentence_tokens = [sent for sent in doc.sents]
+    # Create a text input field for the PDF URL
+    pdf_url = st.text_input("Enter PDF URL:")
 
-        # Calculate sentence scores for summarization
-        sentence_scores = {}
-        for sent in sentence_tokens:
-            for word in sent:
-                if word.text.lower() in word_frequencies.keys():
-                    if sent not in sentence_scores.keys():
-                        sentence_scores[sent] = word_frequencies[word.text.lower()]
-                    else:
-                        sentence_scores[sent] += word_frequencies[word.text.lower()]
+    # Create radio buttons for summarization percentage
+    summarization_percentage = st.radio("Select Summarization Percentage", [10, 20, 25, 30, 50])
 
-        # Calculate the number of sentences to include in the summary
-        select_length = int(len(sentence_tokens) * (summarization_percentage / 100))
+    # Check if the "Summarize" button is clicked
+    if st.button("Summarize"):
+        pdf_text = extract_text_from_url(pdf_url)
+
+        if pdf_text:
+            # Prepare NLP processing
+            nlp = spacy.load('en_core_web_sm')
+            doc = nlp(pdf_text)
+
+            # Calculate word frequencies
+            word_frequencies = calculate_word_frequencies(doc)
+
+            # Tokenize text into sentences
+            sentence_tokens = list(doc.sents)
+
+            # Calculate sentence scores
+            sentence_scores = calculate_sentence_scores(sentence_tokens, word_frequencies)
+
+            # Generate a summary
+            summary = generate_summary(sentence_tokens, sentence_scores, int(len(sentence_tokens) * (summarization_percentage / 100)))
+
+            # Display the summary
+            st.text("Summarized Text:")
+            st.markdown('\n'.join(map(str, summary)))
