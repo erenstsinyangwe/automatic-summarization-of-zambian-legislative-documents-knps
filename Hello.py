6ac71d28735e658b6051f67cfc84983bacf529f2
ernestsinyangwe <107695432+erenstsinyangwe@users.noreply.github.com>
2023-10-31 13:20:17 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,59 +1,44 @@
 import streamlit as st
 
 def run():
-  """
-  Runs the Summarizer-KNPS app.
-  """
+  """Summarize Zambian legislative documents."""
 
-  # Page configuration
+  # Set page configuration
   st.set_page_config(
-    page_title="Automatic Zambian Legislative Documents Summarizer",
-    page_icon="📜"
+    page_title="Zambian Legislative Document Summarizer",
+    page_icon="📜",
   )
 
-  # Set the background color to a light blue
-  st.markdown(
-    """
-    <style>
-    body {
-      background-color: #ADD8E6;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-  )
+  # Set background color
+  st.markdown("""<style>body {background-color: #ADD8E6;}</style>""", unsafe_allow_html=True)
 
   # Welcome message
-  st.markdown(
-    f"""
-    ## Welcome to Summarizer-KNPS, the automatic Zambian legislative documents summarizer!
+  st.markdown("""
+    ## Zambian Legislative Document Summarizer
 
-    With Summarizer-KNPS, you can quickly and easily summarize long and complex legislative documents, saving you time and effort. Simply paste in the link to the PDF document you want to summarize and select the type of summary you want. Summarizer-KNPS will then generate a summary of the document, highlighting the key points and trends.
+    Summarize long and complex Zambian legislative documents quickly and easily.
 
-    **Benefits of using Summarizer-KNPS:**
+    **Benefits:**
 
-    * Save time by automatically summarizing long and complex legislative documents.
-    * Better understand the key points of legislative documents.
-    * Identify key trends and patterns in legislative documents.
-    * Make informed decisions about Zambian legislation.
+    * Save time.
+    * Understand key points better.
+    * Identify key trends and patterns.
+    * Make informed decisions.
 
-    **Try Summarizer-KNPS today and stay informed about Zambian legislation!**
-    """
-  )
+    **Try it today!**
+  """)
 
   # Instructions
-  st.markdown(
-    """
-    **How to use Summarizer-KNPS:**
+  st.markdown("""
+    **How to use:**
 
-    1. Visit the [National Assembly Parliament website](https://www.parliament.gov.zm/acts-of-parliament) and find the PDF document you want to summarize.
+    1. Go to the [National Assembly Parliament website](https://www.parliament.gov.zm/acts-of-parliament) and find the PDF document you want to summarize.
     2. Copy the link to the PDF document.
-    3. Select the type of summary.
+    3. Select the type of summary you want.
     4. Paste the link into the Summarizer-KNPS interface and select the type of summary you want.
     5. Click the "Summarize" button.
     6. Read the summary!
-    """
-  )
+  """)
 
 if __name__ == "__main__":
   run()
