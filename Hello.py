ccebd3cad39f71e686d4147816ef867a5ce3bb31
ernestsinyangwe <107695432+erenstsinyangwe@users.noreply.github.com>
2023-10-31 12:22:26 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,74 +1,55 @@
 import streamlit as st
-import logging
 
-def run():
- """
- Runs the Summarizer-KNPS app.
- """
+def main():
+    """
+    Runs the Summarizer-KNPS app.
+    """
 
- # Logger
- logger = logging.getLogger(__name__)
- logger.setLevel(logging.INFO)
+    # Page configuration
+    st.set_page_config(
+        page_title="Summarizer-KNPS",
+        page_icon="📜"
+    )
 
- # Page configuration
- st.set_page_config(
-   page_title="Summarizer-KNPS",
-   page_icon="📜"
- )
+    # Topbar
+    st.title("Summarizer-KNPS: Your Gateway to Effortless Zambian Legislative Document Summarization")
 
- # Set the background color to a light blue
- st.markdown(
-   """
-   <style>
-   body {
-     background-color: #ADD8E6;
-   }
-   </style>
-   """,
-   unsafe_allow_html=True
- )
+    # Type of summary selection
+    summarization_type = st.selectbox(
+        "Select the type of summary:",
+        ["Abstractive", "Extractive"]
+    )
 
- # Topbar
- st.title("Summarizer-KNPS: Your Gateway to Effortless Zambian Legislative Document Summarization")
+    # Instructions
+    st.markdown(
+        """
+        *How to use Summarizer-KNPS:*
 
- # Type of summary selection
- summarization_type = st.selectbox(
-   "Select the type of summary:",
-   ["Abstractive", "Extractive"]
- )
+        1. Visit the [National Assembly Parliament website](https://www.parliament.gov.zm/acts-of-parliament) and find the PDF document you want to summarize.
+        2. Copy the link to the PDF document.
+        3. Select the type of summary.
+        4. Paste the link into the Summarizer-KNPS interface and select the type of summary you want.
+        5. Click the "Summarize" button.
+        6. Read the summary!
 
- # Instructions
- st.markdown(
-   """
-   **How to use Summarizer-KNPS:**
+        *Benefits of using Summarizer-KNPS:*
 
-   1. Visit the [National Assembly Parliament website](https://www.parliament.gov.zm/acts-of-parliament) and find the PDF document you want to summarize.
-   2. Copy the link to the PDF document.
-   3. Select the type of summary.
-   4. Paste the link into the Summarizer-KNPS interface and select the type of summary you want.
-   5. Click the "Summarize" button.
-   6. Read the summary!
+        - Save time by automatically summarizing long and complex legislative documents.
+        - Better understand the key points of legislative documents.
+        - Identify key trends and patterns in legislative documents.
+        - Make informed decisions about Zambian legislation.
 
-   **Benefits of using Summarizer-KNPS:**
+        *Try Summarizer-KNPS today and stay informed about Zambian legislation!*
+        """
+    )
 
-   - Save time by automatically summarizing long and complex legislative documents.
-   - Better understand the key points of legislative documents.
-   - Identify key trends and patterns in legislative documents.
-   - Make informed decisions about Zambian legislation.
+    # Navigation based on user's selection
+    if summarization_type == "Abstractive":
+        st.markdown("You can navigate to the Abstractive page [here](pages/Abstractive.py).")
+    elif summarization_type == "Extractive":
+        st.markdown("You can navigate to the Extractive page [here](pages/Extractive.py).")
+    else:
+        st.error("Invalid summarization type selected")
 
-   **Try Summarizer-KNPS today and stay informed about Zambian legislation!**
-   """
- )
-
- # Navigation based on user's selection
- if summarization_type == "Abstractive":
-   st.markdown("You can navigate to the Abstractive page [here](pages/Abstractive.py).")
-   logger.info("User selected Abstractive summarization")
- elif summarization_type == "Extractive":
-   st.markdown("You can navigate to the Extractive page [here](pages/Extractive.py).")
-   logger.info("User selected Extractive summarization")
- else:
-   logger.error("Invalid summarization type selected")
-
-if __name__ == "__main__":
- run()
+if _name_ == "_main_":
+    main()
