c0adcd64a8b422c921660b6a2aa6537b6462bee1
ernestsinyangwe <107695432+erenstsinyangwe@users.noreply.github.com>
2023-10-31 13:52:39 +0000

    Update Abstractive.py

--- pages/Abstractive.py
+++ pages/Abstractive.py
@@ -12,7 +12,7 @@ st.set_page_config(
 
 # Declare global variables
 FILE_CONTENT = None
-CHECKPOINT = "nsi319/legal-led-base-16384"
+CHECKPOINT = "nsi319/legal-pegasus"
 TOKENIZER = AutoTokenizer.from_pretrained(CHECKPOINT)
 MODEL = AutoModelForSeq2SeqLM.from_pretrained(CHECKPOINT)
 
