14cec0784b1a59fa6bcbb5e9782f40fb7d141718
ernestsinyangwe <107695432+erenstsinyangwe@users.noreply.github.com>
2023-10-31 13:45:23 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -14,8 +14,7 @@ def run():
 
   # Welcome message
   st.markdown("""
-    ## Zambian Legislative Document Summarizer
-    ### Summarizer-KNPS
+    ## Zambian Legislative Document Summarizer: Summarizer-KNPS
 
     Summarize long and complex Zambian legislative documents quickly and easily.
 
