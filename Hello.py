4e12b97509bf08d18095ac300dfe5a887b03a7d3
ernestsinyangwe <107695432+erenstsinyangwe@users.noreply.github.com>
2023-10-31 13:27:49 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,44 +1,49 @@
 import streamlit as st
 
 def run():
-  """Summarize Zambian legislative documents."""
+    """
+    Summarize Zambian legislative documents using Summarizer-KNPS.
+    """
 
-  # Set page configuration
-  st.set_page_config(
-    page_title="Zambian Legislative Document Summarizer",
-    page_icon="📜",
-  )
+    # Set page configuration
+    st.set_page_config(
+        page_title="Zambian Legislative Document Summarizer",
+        page_icon="📜"
+    )
 
-  # Set background color
-  st.markdown("""<style>body {background-color: #ADD8E6;}</style>""", unsafe_allow_html=True)
+    # Set background color
+    st.markdown("""
+        <style>body {background-color: #ADD8E6;}</style>
+    """, unsafe_allow_html=True)
 
-  # Welcome message
-  st.markdown("""
-    ## Zambian Legislative Document Summarizer
+    # Welcome message
+    st.markdown("""
+        # Zambian Legislative Document Summarizer
+        ## Summarizer-KNPS
 
-    Summarize long and complex Zambian legislative documents quickly and easily.
+        Quickly and easily summarize long and complex Zambian legislative documents.
 
-    **Benefits:**
+        *Benefits:*
 
-    * Save time.
-    * Understand key points better.
-    * Identify key trends and patterns.
-    * Make informed decisions.
+        - Save time.
+        - Improve understanding of key points.
+        - Discover key trends and patterns.
+        - Make informed decisions.
 
-    **Try it today!**
-  """)
+        *Try it today!*
+    """)
 
-  # Instructions
-  st.markdown("""
-    **How to use:**
+    # Instructions
+    st.markdown("""
+        *How to use:*
 
-    1. Go to the [National Assembly Parliament website](https://www.parliament.gov.zm/acts-of-parliament) and find the PDF document you want to summarize.
-    2. Copy the link to the PDF document.
-    3. Select the type of summary you want.
-    4. Paste the link into the Summarizer-KNPS interface and select the type of summary you want.
-    5. Click the "Summarize" button.
-    6. Read the summary!
-  """)
+        1. Visit the [National Assembly Parliament website](https://www.parliament.gov.zm/acts-of-parliament) and find the PDF document you want to summarize.
+        2. Copy the link to the PDF document.
+        3. Choose the type of summary you want.
+        4. Paste the link into the Summarizer-KNPS interface and select your desired summary type.
+        5. Click the "Summarize" button.
+        6. Read the summary!
+    """)
 
-if __name__ == "__main__":
-  run()
+if _name_ == "_main_":
+    run()
