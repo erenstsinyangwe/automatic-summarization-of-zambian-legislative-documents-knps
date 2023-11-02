7b0c43fbf4172f2e7a792d01db3ad7ac5b7dc75a
ernestsinyangwe <107695432+erenstsinyangwe@users.noreply.github.com>
2023-11-02 04:26:28 +0000

    Update Abstractive.py

--- pages/Abstractive.py
+++ pages/Abstractive.py
@@ -1,86 +1,34 @@
-import subprocess
 import streamlit as st
-import requests
-from pdfminer.high_level import extract_text
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import pipeline
 
-# Install required packages (if not already installed)
-subprocess.run(
-    [
-        "pip",
-        "install",
-        "transformers[sentencepiece]",
-        "pdfminer.six",
-        "torch",
-        "torchvision",
-        "torchaudio",
-        "torch",
-        "transformers",
-        "spacy",
-        "numpy",
-    ],
-    check=True,
-)
-
-# Function to extract text from a PDF file
-def extract_text_from_pdf(pdf_file_path):
-    response = requests.get(pdf_file_path)
-    with open("temp.pdf", "wb") as pdf_file:
-        pdf_file.write(response.content)
-    return extract_text("temp.pdf").strip()
-
-# Initialize the pdf_text variable
-pdf_text = None
+# Load the Hugging Face models
+checkpoint = "facebook/bart-large-cnn"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
 
 # Streamlit app
 st.title("Text Summarizer")
 
-# User input for PDF link or text
-input_type = st.radio("Choose input type:", ("PDF Link", "Text Input"))
+# User input for text
+text_input = st.text_area("Enter the text:")
 
-if input_type == "PDF Link":
-    pdf_file_path = st.text_input("Enter the link to the PDF file:")
+# Summarize the text
+if st.button("Summarize"):
+    with st.empty():
+        st.text("Summarizing... Please wait.")
 
-    if st.button("Summarize"):
-        with st.empty():
-            st.text("Summarizing... Please wait.")
+        # Tokenize the text
+        tokenized_text = tokenizer.encode(text_input, return_tensors="pt")
 
-            try:
-                pdf_text = extract_text_from_pdf(pdf_file_path)
-            except Exception as e:
-                st.error(f"Summarization failed: {str(e)}")
-                pdf_text = None
-else:
-    text_input = st.text_area("Enter the text:")
+        # Generate the summary
+        output = model.generate(tokenized_text)
 
-    if st.button("Summarize"):
-        with st.empty():
-            st.text("Summarizing... Please wait.")
-            pdf_text = text_input
-
-# Check if the pdf_text variable is empty
-if pdf_text is not None:
-    # Initialize Hugging Face models
-    #checkpoint = "nsi319/legal-pegasus"
-    checkpoint = "facebook/bart-large-cnn"
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-    model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
-
-    # Tokenize and summarize the text
-    sentences = pdf_text.split(".")
-    chunks = [
-        sentence
-        for sentence in sentences
-        if len(tokenizer.tokenize(sentence)) + len(tokenizer.tokenize("Summary:"))
-        <= tokenizer.model_max_length
-    ]
-
-    # Generate and display the model's output for each input
-    st.header("Summarized Text:")
-    for chunk in chunks:
-        output = model.generate(tokenizer.encode("Summary: " + chunk, return_tensors="pt"))
+        # Decode the summary
         generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+        # Display the summary
+        st.header("Summarized Text:")
         st.write(generated_text)
 
-    st.text("Summarization complete.")
+        st.text("Summarization complete.")
