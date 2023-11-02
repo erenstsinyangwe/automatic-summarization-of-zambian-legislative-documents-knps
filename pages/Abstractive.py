d713001070cab1f4064c78e923f5e8d6b2c38cb1
ernestsinyangwe <107695432+erenstsinyangwe@users.noreply.github.com>
2023-11-02 03:25:21 +0000

    Update Abstractive.py

--- pages/Abstractive.py
+++ pages/Abstractive.py
@@ -5,10 +5,9 @@ from pdfminer.high_level import extract_text
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 # Install required packages
-subprocess.run(['pip', 'install', 'transformers[sentencepiece]', 'pdfminer.six'])
-subprocess.run(['pip', 'install', 'torch'])
+subprocess.run(['pip', 'install', 'transformers[sentencepiece]', 'pdfminer.six', 'torch'])
 
-# Define a function to extract text from a PDF file
+# Function to extract text from a PDF file
 def extract_text_from_pdf(pdf_file_path):
     response = requests.get(pdf_file_path)
     with open("temp.pdf", "wb") as pdf_file:
@@ -20,6 +19,7 @@ st.title("Text Summarizer")
 
 # User input for PDF link or text
 input_type = st.radio("Choose input type:", ("PDF Link", "Text Input"))
+
 if input_type == "PDF Link":
     pdf_file_path = st.text_input("Enter the link to the PDF file:")
     if st.button("Summarize"):
@@ -46,11 +46,7 @@ if pdf_text is not None:
 
     # Tokenize and summarize the text
     sentences = pdf_text.split(".")
-    chunks = []
-    for sentence in sentences:
-        combined_length = len(tokenizer.tokenize(sentence)) + len(tokenizer.tokenize("Summary:"))
-        if combined_length <= tokenizer.model_max_length:
-            chunks.append(sentence)
+    chunks = [sentence for sentence in sentences if len(tokenizer.tokenize(sentence)) + len(tokenizer.tokenize("Summary:")) <= tokenizer.model_max_length]
 
     # Generate and display the model's output for each input
     st.header("Summarized Text:")
@@ -60,9 +56,6 @@ if pdf_text is not None:
         st.write(generated_text)
     st.text("Summarization complete.")
 
-# Define pdf_text
-pdf_text = None  # Initialize pdf_text as None
-
 # Run Streamlit app
-if __name__ == "__main__":
-    st.run_app()
+if _name_ == "_main_":
+    st.run_app()
