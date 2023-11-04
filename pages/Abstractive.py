a10a9cf6457b59aae500ed0b60a5e4d3bb5728b8
Ernest Sinyangwe <ernest.s.sinyangwe@gmail.com>
2023-11-04 20:09:01 +0200

    fix

--- pages/Abstractive.py
+++ pages/Abstractive.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import requests
 import re
-import nltk
-from nltk.tokenize import word_tokenize, sent_tokenize
 import string
 from pdfminer.high_level import extract_text
+import nltk
 
 # Set custom CSS for better styling
 st.write(
@@ -30,6 +29,7 @@ st.write(
     unsafe_allow_html=True
 )
 
+# Download NLTK data
 nltk.download("punkt")
 
 def extract_pdf_text(pdf_link: str) -> str:
@@ -65,8 +65,8 @@ if st.button("Summarize"):
             st.error(f"Error: {e}")
         else:
             def summarize(text: str, per: float) -> str:
-                sentences = sent_tokenize(text)
-                tokens = word_tokenize(text)
+                sentences = nltk.sent_tokenize(text)
+                tokens = nltk.word_tokenize(text)
                 stopwords = set(string.punctuation + " ")
                 word_frequencies = {}
                 
@@ -78,7 +78,7 @@ if st.button("Summarize"):
                         else:
                             word_frequencies[token] += 1
                 
-                scores = {sentence: sum(word_frequencies.get(word, 0) for word in word_tokenize(sentence)) for sentence in sentences}
+                scores = {sentence: sum(word_frequencies.get(word, 0) for word in nltk.word_tokenize(sentence)) for sentence in sentences}
                 important_sentences = sorted(sentences, key=lambda sentence: scores[sentence], reverse=True)
                 
                 num_sentences = max(1, int(per / 100 * len(sentences)))  # Ensure at least 1 sentence is selected
